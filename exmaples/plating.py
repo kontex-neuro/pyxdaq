51fe211519b46343d2e40da75d8e393abeedd537
stonelin <jason840708@gmail.com>
2024-01-19 17:40:16 +0800

    Add notes for example

--- exmaples/plating.py
+++ exmaples/plating.py
@@ -12,45 +12,79 @@ print(xdaq.ports)
 
 
 def pluses(mA: float, frequency: float):
+    """
+    Create a basic pluse with duty cycle 50%, current can be positive or negative.
+         |-----|     |-----|
+         |     |     |     |
+    -----|     |-----|     |-----
+
+    |--period--| = 1/frequency
+    """
     half_period_ms = 1e3 / frequency / 2
     return (lambda **kwargs: kwargs)(
+        # Polarity of the first phase
         polarity=StartPolarity.cathodic if mA < 0 else StartPolarity.anodic,
+        # Shape of the pluse
         shape=StimShape.Monophasic,
+        # Delay between the trigger and the start of the pluse
         delay_ms=0,
+        # Duration of the first phase, there are only one phase in Monophasic shape
         phase1_ms=half_period_ms,
         phase2_ms=0,
         phase3_ms=0,
+        # Use 10uA step size, the current will be truncated to the nearest 10uA
+        step_size=StimStepSize.StimStepSize10uA,
+        # Amplitude of the first phase
         amp_phase1_mA=mA,
         amp_phase2_mA=0,
+        # Please refer to Intan Manual for ampsettle and charge recovery
         pre_ampsettle_ms=0,
         post_ampsettle_ms=half_period_ms,
         post_charge_recovery_ms=0,
-        pulses=1,
+        # The duration after the pluse before the next pluse
         post_pluse_ms=half_period_ms,
+        # Sending pluses continuously when the trigger is high
         trigger=TriggerEvent.Level,
         trigger_pol=TriggerPolarity.High,
-        step_size=StimStepSize.StimStepSize10uA,
+        # Since we are using Level trigger, sending one pluse each time
+        pulses=1,
     )
 
 
 def send_pulses(xdaq: XDAQ, stream, channel, duration_ms, pluse_current_mA, pluse_frequency):
     software_trigger_id = 1
+
+    # The enable_stim function will return a function to disable the stim
     disable_stim = enable_stim(
         xdaq=xdaq,
         stream=stream,
         channel=channel,
+        # Trigger source, 24~31 is the software trigger 1~8
         trigger_source=24 + software_trigger_id - 1,
         **pluses(pluse_current_mA, pluse_frequency)
     )
+
+    # Enable software trigger
     xdaq.manual_trigger(software_trigger_id, True)
+
+    # Calculate the number of steps to run, the number of steps should be multiple of 128 to avoid alignment error
     run_steps = (int(duration_ms / 1000 * xdaq.sampleRate.rate) + 127) // 128 * 128
+
+    # Set the maximum number of steps to run
+    # this could be replaced by
+    # xdaq.readDataBlock(run_steps).to_samples(),
+    # But it will take longer time to run, since it will read data from FPGA
     xdaq.setMaxTimeStep(run_steps)
     xdaq.setContinuousRunMode(False)
     xdaq.run()
     while xdaq.is_running():
         time.sleep(0.1)
+
+    # Disable software trigger after the run
     xdaq.manual_trigger(software_trigger_id, False)
+    # Disable the stim, a stim can run multiple times, here we set the stim every time for demonstration
     disable_stim()
+
     return run_steps
 
 
