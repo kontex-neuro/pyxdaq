Fix impedance measurement issues, enhance precision
- Correct slicing bug blocking impedance measurements.
- Stabilize phase measurements.
- Use direct calculation over FFT for target frequency, boosting accuracy.

pyxdaq/impedance.py
@@ -8,15 +8,19 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def amplitudeOfFreqComponent(data, sampleRate, frequency):
-    data_segment = np.array(data)
-    fft_result = np.fft.rfft(data_segment, norm='forward')
-    freqs = np.fft.fftfreq(len(data_segment), d=1 / sampleRate)
+def amplitudeOfFreqComponent(data, sample_rate, frequency):
+    if len(data) % 2 != 0:
+        data = data[:-1]  # Truncate the last sample for even length
 
-    # Find the closest frequency bin to the frequency of interest
-    target_index = np.argmin(np.abs(freqs - frequency))
+    n = np.arange(len(data))
+    cos_wave = np.cos(2 * np.pi * frequency * n / sample_rate)
+    sin_wave = -np.sin(2 * np.pi * frequency * n / sample_rate)
 
-    return fft_result[target_index] * 2
+    # Compute the real and imaginary parts
+    Re = np.dot(data, cos_wave)
+    Im = np.dot(data, sin_wave)
+    r = (Re + 1j * Im) * 2
+    return r / len(data)
 
 
 def measureComplexAmplitude(ampdata: np.ndarray, sampleRate: int, frequency: float) -> np.ndarray:
@@ -144,7 +148,7 @@ class MeasurementStrategy:
     _periods: int = None
     _duration: float = None
 
-    _min_periods: int = 5 # less than 5 periods is not enough for accurate measurement
+    _min_periods: int = 5  # less than 5 periods is not enough for accurate measurement
     _min_duration: float = None
 
     _max_duration: float = 1

pyxdaq/xdaq.py
@@ -1309,7 +1309,7 @@ class XDAQ:
         #         0         1       2        3       4
         #   (zscale, wave pin, signal, channel, stream)
         # -> zscale, wave pin, stream, channel, signal
-        all_data = np.array(all_data).transpose(0, 1, 4, 3, 2)
+        all_data = np.array(all_data).transpose(0, 1, 4, 3, 2)[..., :period * num_periods]
         n_zscale, n_test_ch, n_stream, _, signal_length = all_data.shape
         assert n_zscale == 3
         # extract only signals from the target channel which the testing signal is applied
@@ -1323,11 +1323,11 @@ class XDAQ:
             return target_channel_data
 
         # -> zscale, stream * target_channel, signal
-        measured_signal = target_channel_data[:, :, 3 + 2 * period:3 - period].reshape(
-            (3, -1, signal_length)
+        signal_to_measure = target_channel_data[..., 3 + 2 * period:3 - period].reshape(
+            (n_zscale, n_stream * n_test_ch, -1)
         )
         magnitude, phase = calculate_impedance(
-            measured_signal,
+            signal_to_measure,
             sample_rate,
             rhs=self.rhs,
             test_frequency=test_frequency,